4666dc8d81a4280e880f6ced31834c3770140a20
OliverL <60389050+Snubb@users.noreply.github.com>
2023-04-12 22:40:22 +0200

    Some random commenting :)

--- src/Position.java
+++ src/Position.java
@@ -33,14 +33,15 @@ public class Position {
         return new Position(x + 1, y);
     }
 
-    public boolean equals(Object obj) {
-        if (obj.getClass() != Position.class) {
-            return false;
-        }
-
-        return ((Position) obj).x == this.x && ((Position) obj).y == this.y;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
     }
 
+    @Override
     public int hashCode() {
         return Objects.hash(x, y);
     }
--- src/RandomRobot.java
+++ src/RandomRobot.java
@@ -11,21 +11,25 @@ public class RandomRobot {
         this.previousPosition = this.position;
     }
 
+    // Moves the robot in a random valid direction that is not its previous position
+    // Will move to its previous position if it can't move anywhere else
     public void move() {
         Position[] positions = {
                 position.getPosToNorth(),
                 position.getPosToSouth(),
                 position.getPosToEast(),
                 position.getPosToWest()
-        };
+        }; // All potential movement options
 
         ArrayList<Position> possibleMoves = new ArrayList<>();
 
+        // Add a movement option to the possibleMoves list if it is a valid option
         for (Position pos :
                 positions) {
             if (maze.isMovable(pos) && !pos.equals(previousPosition)) possibleMoves.add(pos);
         }
 
+        // Randomize a move. If there are no possible moves, move back
         if (possibleMoves.size() > 0) {
             int randomIndex = (int)(Math.random() * (possibleMoves.size()));
             setPosition(possibleMoves.get(randomIndex));
